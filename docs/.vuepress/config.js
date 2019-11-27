5156989fe967390bf8824c33f1b9940d50d0efeb
polly-zou <310663565@qq.com>
2019-11-27 19:26:45 +0800

    press

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'Press',
+  description: 'Press',
+  head: [
+    // 这里的 '/' 指向 docs/.vuepress/public 文件目录 
+    ['link', {
+      rel: 'icon',
+      href: '/img/witch.ico'
+    }]
+  ]
 }
