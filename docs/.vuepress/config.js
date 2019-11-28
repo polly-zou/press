914d562a60598a3783e04a1d1efdb9b80fe3b814
polly-zou <310663565@qq.com>
2019-11-28 11:25:31 +0800

    press

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,6 @@
+const sidebar = require('./config/sidebar');
+const nav = require('./config/nav');
+const routeConfig = require('./plugins/routeConfig');
 module.exports = {
   title: 'Press',
   description: 'Press',
@@ -7,5 +10,18 @@ module.exports = {
       rel: 'icon',
       href: '/img/witch.ico'
     }]
-  ]
+  ],
+  port:3001,
+  // temp:指定客户端文件的临时目录
+   // 顶部导航栏
+  themeConfig: {
+    displayAllHeaders: true, //展开侧边栏所有的小标题
+    sidebarDepth: 1, //提取markdown中h2标题，作为小标题显示在侧边栏上。
+    lastUpdated: '最后更新', // 文档更新时间：每个文件git最后提交的时间,
+    nav, //导航
+    sidebar, //侧边栏
+  },
+  plugins: {
+    routeConfig
+  }
 }
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,69 @@
+const nav = [
+  {
+    text: "WEB",
+    items: [{
+      text: 'SVG',
+        items: [{
+          text: '基础',
+          link: '/PAGE/SVG/',
+        }]
+    },
+    {
+      text: 'Javascript',
+        items: [{
+          text: '基础',
+          link: '/PAGE/Javascript/',
+        }]
+    },
+    {
+      text: 'Http',
+        items: [{
+          text: '基础',
+          link: '/PAGE/Http/',
+        }]
+    },
+    {
+      text: 'Less',
+        items: [{
+          text: '基础',
+          link: '/PAGE/Less/',
+        }]
+    }
+  ]
+  },
+  {
+    text: "server",
+    items: [{
+
+    }]
+  },
+  {
+    text: "算法",
+    items: [
+    {
+      text: '时间复杂度和空间复杂度',
+        items: [{
+          text: '时间复杂度',
+          link: '',
+        },
+        {
+          text: '空间复杂度',
+          link: '',
+        }
+      ]
+    },
+    {
+      text: '十大排序',
+        items: [{
+          text: '快速排序',
+          link: '',
+        },
+        {
+          text: '插入排序',
+          link: '',
+        }
+      ]
+    }    
+  ]
+  }
+]
