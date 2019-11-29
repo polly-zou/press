42dce9a74c9cbe67c47466a8b332bd9b3eef09ef
polly-zou <310663565@qq.com>
2019-11-29 12:01:42 +0800

    pres

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,13 +1,26 @@
-const nav = [{
-  title: "WEB",
-  children: [
-    {
-      title: "SVG",
-      children:[]
-    },
-    {
-      title: "JavaScript",
-      children:[]
-    },
-  ]
-}]
+const nav = [
+  {
+    text: "博客指南",
+    items: [{
+      text: 'WEB',
+      items: [{
+          text: 'SVG基础',
+          link: '/PAGE/2019-11-29-svg-基础/',
+        },
+        {
+          text: 'Javascript',
+          link: '/PAGE/2019-11-29-js-reg/',
+        },
+      ]
+    }]
+  },
+  {
+    text: "工具",
+    items: [
+      {
+        text: 'Emoji表情',
+        link: '/PAGE/2019-11-20-git-emoji/',
+      }
+    ]
+  }
+]
